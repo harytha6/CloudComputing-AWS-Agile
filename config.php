6e1c3ca0294275bfe9912b795985602c69a57788
Shivakumar <shivakumar.haralahallimariyappa@stud.fra-uas.de>
2022-01-09 19:39:51 +0100

    added MAP Dashboard

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $hostname = "localhost";
 $username = "root";
 $password = "";
-$database = "abc";
+$database = "test";
 
 $conn = mysqli_connect($hostname, $username, $password, $database) or die("Database connection failed");
 
