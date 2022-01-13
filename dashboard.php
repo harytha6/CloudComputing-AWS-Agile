918c35a1d0f9af7eb7669c2fff1e79e49d754865
bryan26-xyz <26bilal@gmail.com>
2022-01-13 18:11:38 +0530

    Revert "Update dashboard.php"
    This reverts commit 51ba8e8f4db0dbee25299aa9b7c12943a9af4a43.

--- dashboard.php
+++ dashboard.php
@@ -48,7 +48,7 @@ if (mysqli_num_rows($check)>0) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<!-- Test -->
+
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous"> 
 
 
